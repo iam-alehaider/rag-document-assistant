9e67750bbf776ab64acbbc94cb7a73f27a1ef83e
Ale Haider <ale.haider498@gmail.com>
2026-07-10 23:19:27 +0500

    Fix Alembic DATABASE_URL interpolation issue

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -16,7 +16,10 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 settings = get_settings()
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL.replace("%", "%%")
+)
 
 target_metadata = Base.metadata
 
